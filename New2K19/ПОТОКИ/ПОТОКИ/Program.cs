030de1c8de25a3dc2d349cfe5e5e57aa8a5d9da1
AlikhanTortay <47249375+AlikhanTortay@users.noreply.github.com>
2019-03-30 01:12:37 +0600

    Add files via upload

---
+++ New2K19/ПОТОКИ/ПОТОКИ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading; //Именно это пространство имен поддерживает многопоточность
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(func); //Создаем новый объект потока (Thread)
+
+            myThread.Start(); //запускаем поток
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread 1 deduce " + i);
+                Thread.Sleep(0);
+            }
+
+            Console.Read(); //Приостановим основной поток
+
+        }
+
+        //Функция запускаемая из другого потока
+        static void func()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread 2 deduce" + i.ToString());
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
