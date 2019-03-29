f2f890e839e62caeb4cc086e342d16c8496cdd92
AlikhanTortay <47249375+AlikhanTortay@users.noreply.github.com>
2019-03-29 22:38:54 +0600

    Add files via upload

---
+++ New2K19/quiz2-1/quiz2-1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+[Serializable]
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Movie FavouriteMovie { get; set; }
+
+    public Person()
+    { }
+
+    public Person(string name, int age, Movie movie)
+    {
+        Name = name;
+        Age = age;
+        FavouriteMovie = movie;
+    }
+}
+
+
+
+[Serializable]
+public class Movie
+{
+
+    public string Title { get; set; }
+    public int ReleaseYear { get; set; }
+
+    public Movie() { }
+
+    public Movie(string title, int releaseYear)
+    {
+        Title = title;
+        ReleaseYear = releaseYear;
+    }
+
+
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Movie firstMovie = new Movie("Emoji movie", 2017);
+        Movie secondMovie = new Movie("50 shades of gray", 2015);
+        Movie thirdMovie = new Movie("Shrek", 1999);
+        Movie fourthMovie = new Movie("NinjaCop", 1987);
+
+        Person akbar = new Person("Akbar", 29, secondMovie);
+        Person ramesh = new Person("Ramesh", 20, firstMovie);
+        Person durga = new Person("Durga", 43, fourthMovie);
+        Person daler = new Person("Daler", 38, thirdMovie);
+
+
+
+        var array = new Person[] { akbar, ramesh, durga, daler };
+        // foreach (var item in array)
+        // {
+        //     System.Console.WriteLine(item.Age.ToString() + item.Name);
+        // }
+
+
+        XmlSerializer formatter = new XmlSerializer(typeof(Person[]));
+
+        using (FileStream fs = new FileStream("persons.xml", FileMode.OpenOrCreate))
+        {
+            formatter.Serialize(fs, array);
+        }
+
+        using (FileStream fs = new FileStream("persons.xml", FileMode.OpenOrCreate))
+        {
+            Person[] newPerson = (Person[])formatter.Deserialize(fs);
+
+
+            foreach (var item in array)
+            {
+                System.Console.WriteLine(item.Age.ToString() + item.Name);
+            }
+
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ New2K19/quiz2-2/quiz2-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.BackgroundColor = ConsoleColor.Magenta;
+        Console.ForegroundColor = ConsoleColor.Black;
+        new Thread(changeBackground).Start();
+        string someString = "Is this a real life, or is this just fantasy?";
+
+        for (int i = 0; i <= someString.Length; i++)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine(someString.Substring(0, i));
+            Thread.Sleep(100);
+            Console.Clear();
+        }
+    }
+    static void changeBackground()
+    {
+
+        while (true)
+        {
+            if (Console.BackgroundColor == ConsoleColor.Magenta)
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Magenta;
+            }
+            Thread.Sleep(200);
+        }
+    }
+
+}
