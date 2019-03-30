85961731de5f06a36c9c93786d0cc74cd8f6e273
AlikhanTortay <47249375+AlikhanTortay@users.noreply.github.com>
2019-03-30 10:44:04 +0600

    Add files via upload

---
+++ New2K19/Түзу/AAAAAAAAAAAAAAAAA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AAAAAAAAAAAAAAAAA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startposx = 20;
+            int startposy = 20;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int x = 0; x < 10; x++)
+            {
+                int y = (x + 2) * -1;//считаем уравнение
+                Console.SetCursorPosition(startposx + x, startposy + y);
+                Console.Write("*");
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
